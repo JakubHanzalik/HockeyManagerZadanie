d103928d53efd7d3e1a5736e234aa37960c86d9f
Jakub Hanzalik <jakub.hanzalik@gmail.com>
2024-06-06 22:08:00 +0200

    Add project files.

---
+++ HockeyManagerZadanie/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace HockeyManagerZadanie
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // TODO: Implement your code here
+
+            Console.WriteLine("Hello, world!");
+        }
+    }
+}
